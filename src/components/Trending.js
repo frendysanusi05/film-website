23ad4fe2342c2c24d46ec242ac4a586ca00e41b9
frendysanusi05 <frendyvivo5@gmail.com>
2023-06-05 12:42:49 +0700

    Update landing page

---
+++ src/components/Trending.js
@@ -0,0 +1,125 @@
+import { Card, Container, Row, Col, Image } from 'react-bootstrap'
+import duneImage from '../assets/images/trending/dune.jpg'
+import everythingImage from '../assets/images/trending/everything.jpg'
+import infiniteImage from '../assets/images/trending/infinite.jpg'
+import jokerImage from '../assets/images/trending/joker.jpg'
+import lightyearImage from '../assets/images/trending/lightyear.jpg'
+import morbiusImage from '../assets/images/trending/morbius.jpg'
+
+const Trending = () => {
+    return (
+        <div>
+            <Container>
+                <br/>
+                <h1 className="text-white">TRENDING MOVIES</h1>
+                <br/>
+                <Row>
+                    <Col md={4} className="movieWrapper" id="trending">
+                        <Card className="text-white movieImage">
+                            <Image src={ duneImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">DUNE</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="text-white movieImage">
+                            <Image src={ everythingImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">EVERYTHING</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="text-white movieImage">
+                            <Image src={ infiniteImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">INFINITE</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="text-white movieImage">
+                            <Image src={ jokerImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">JOKER</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="text-white movieImage">
+                            <Image src={ lightyearImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">LIGHTYEAR</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="text-white movieImage">
+                            <Image src={ morbiusImage } alt="Dune Movies" className="images" />
+                            <div className="bg-dark">
+                                <div className="m-2 p-1">
+                                    <Card.Title className="text-center">MORBIUS</Card.Title>
+                                    <Card.Text className="text-left">
+                                        This is a wider card with supporting text below as a natural lead-in
+                                        to additional content.
+                                    </Card.Text>
+                                    <Card.Text className="text-left">
+                                        Last updated 3 mins ago
+                                    </Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}   
+
+export default Trending
